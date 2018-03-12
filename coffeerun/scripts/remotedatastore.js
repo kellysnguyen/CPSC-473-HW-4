fbdead547a12eb3513beccceaf8de46a86a99203
Kelly Nguyen <kelly@Kellys-MBP.att.net>
2018-03-11 21:34:27 -0700

    first commit

---
+++ coffeerun/scripts/remotedatastore.js
@@ -0,0 +1,38 @@
+(function(window) {
+  "use strict";
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function RemoteDataStore(url) {
+    if (!url) {
+      throw new Error("No remote URL supplied.");
+    }
+    this.serverUrl = url;
+  }
+  RemoteDataStore.prototype.add = function(key, val) {
+    $.post(this.serverUrl, val);
+  };
+  RemoteDataStore.prototype.getAll = function(cb) {
+    $.get(this.serverUrl, function(serverResponse) {
+      console.log(serverResponse);
+
+      cb(serverResponse);
+    });
+  };
+  RemoteDataStore.prototype.get = function(key, cb) {
+    $.get(this.serverUrl + "?emailAddress=" + key, function(serverResponse) {
+      console.log(serverResponse[0]);
+      cb(serverResponse);
+    });
+  };
+  RemoteDataStore.prototype.remove = function(key) {
+    $.get(this.serverUrl + "?emailAddress=" + key, function(serverResponse) {
+      var myID = (serverResponse[0].id);
+      $.ajax("http://localhost:2403/coffeeorders" + "/" + myID, {
+        type: "DELETE"
+      });
+    });
+  };
+  App.RemoteDataStore = RemoteDataStore;
+  window.App = App;
+})(window);
